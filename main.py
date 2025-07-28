3688a8189b34c6aba3a264879a93f81921bdc9e1
mattoz <nick.frame76@outlook.it>
2025-07-28 12:03:42 +0200

    11.5\

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ from minio import Minio
 import re
 import toml
 
-APP             = Flask(__name__)
-MINIO           = Minio(endpoint=os.environ['MINIO_URL'], access_key=os.environ['MINIO_USER'], secret_key=os.environ['MINIO_PSW'],
-                    secure=False)
-TMP_FOLDER      = os.path.join('.', 'tmp')
-OUTPUT_BUCKET   = 'output'
+APP = Flask(__name__)
+MINIO = Minio(endpoint=os.environ['MINIO_URL'], access_key=os.environ['MINIO_USER'], secret_key=os.environ['MINIO_PSW'],
+              secure=False)
+TMP_FOLDER = os.path.join('.', 'tmp')
+OUTPUT_BUCKET = 'output'
 
-DBC_VOLUME      = os.environ['DBC_VOLUME']
+DEFAULT_CONFIG_PATH = os.path.join('.', 'resources', 'config.toml')
+DEFAULT_DBC_PATH = os.path.join('.', 'resources', '11-bit-OBD2-v4.0.dbc')
 
-CONFIG_VOLUME       = os.environ['CONFIG_VOLUME']
-CONFIG_PATH         = os.environ['CONFIG_PATH']
+DBC_VOLUME = os.environ['DBC_VOLUME']
+
+CONFIG_VOLUME = os.environ['CONFIG_VOLUME']
+CONFIG_PATH = os.environ['CONFIG_PATH']
 
 DATA = {}
+
 
 @APP.route(rule='/mdf-handle', methods=['POST'])
 def handle():
@@ -56,14 +60,14 @@ def handle_mdf(mdf: MDF, name: str):
         APP.logger.warning("No config found, using default DBC file")
     dbc_volume = config['dbc']['volume'] if config and 'dbc' in config and 'volume' in config['dbc'] else '/dbc'
     dbc_files = []
-    
+
     if config and 'dbc' in config and 'src' in config['dbc']:
         for dbc_src in config['dbc']['src']:
             dbc_files.append((os.path.join(dbc_volume, dbc_src['filepath']), dbc_src['can_bus_channel']))
     else:
         # Fallback to default DBC file in /dbc volume
         dbc_files.append((os.path.join('/dbc', '11-bit-OBD2-v4.0.dbc'), 0))
-    
+
     decoded = mdf.extract_bus_logging(
         database_files={
             "CAN": dbc_files
@@ -85,16 +89,20 @@ def get_mdf_path(file_name: str, io: Literal['input', 'output'], extension: Lite
     else:
         raise Exception("Invalid IO id")
 
+
 def basename(path):
     rgx = r'^(.+?)(?:\.[^.]+)?$'
     return re.match(rgx, path).group(1)
 
+
 def setup():
     os.makedirs(TMP_FOLDER, exist_ok=True)
+
 
 def fetch_config():
     with open(CONFIG_PATH, 'r') as f:
         DATA['config'] = toml.load(f)
+
 
 def get_config():
     return DATA.get('config')
@@ -103,53 +111,38 @@ def get_config():
 def copy_defaults(config_path):
     config_ok = True
     dbc_ok = True
-    
+
     if os.path.exists(config_path):
         APP.logger.info(f"Configuration file found at {config_path}")
     else:
-        default_config_path = os.path.join('resources', 'config.toml')
-        if os.path.exists(default_config_path):
-            os.makedirs(os.path.dirname(config_path), exist_ok=True)
-            
-            with open(default_config_path, 'r') as src, open(config_path, 'w') as dst:
-                dst.write(src.read())
-            
-            APP.logger.info(f"Default configuration copied to {config_path}")
-        else:
-            APP.logger.error(f"Default configuration not found at {default_config_path}")
-            config_ok = False
-    
-    default_dbc_filename = '11-bit-OBD2-v4.0.dbc'
-    default_dbc_path = os.path.join('.', 'resources', default_dbc_filename)
-    target_dbc_path = os.path.join(DBC_VOLUME, default_dbc_filename)
+        config_ok = copy_default(DEFAULT_DBC_PATH, config_path, 'config')
 
-    all_entries = os.listdir(os.path.join('.', 'resources'))
-    bll_entries = os.listdir(os.path.join('.'))
+    target_dbc_path = os.path.join(DBC_VOLUME, os.path.basename(DEFAULT_DBC_PATH))
 
-    print(all_entries)
-    print(bll_entries)
-    
     if os.path.exists(target_dbc_path):
         APP.logger.info(f"DBC file found at {target_dbc_path}")
     else:
-        if os.path.exists(default_dbc_path):
-            os.makedirs(os.path.dirname(target_dbc_path), exist_ok=True)
-            
-            with open(default_dbc_path, 'rb') as src, open(target_dbc_path, 'wb') as dst:
-                dst.write(src.read())
-            
-            APP.logger.info(f"Default DBC file copied to {target_dbc_path}")
-        else:
-            APP.logger.error(f"Default DBC file not found at {default_dbc_path}")
-            dbc_ok = False
-    
+        dbc_ok = copy_default(DEFAULT_CONFIG_PATH, target_dbc_path, 'DBC')
+
     return config_ok and dbc_ok
 
 
-if __name__ == '__main__':
-    path = './resources/11-bit-OBD2-v4.0.dbc'
-    print(os.path.exists(path))
+def copy_default(src: str, dst: str, ftype: Literal['DBC', 'config']) -> bool:
+    ok = True
+    if os.path.exists(src):
+        os.makedirs(os.path.dirname(dst), exist_ok=True)
 
+        with open(src, 'r') as src, open(dst, 'w') as dst:
+            dst.write(src.read())
+
+        APP.logger.info(f"Default {ftype} file copied to {dst}")
+    else:
+        APP.logger.error(f"Default {ftype} file not found at {src}")
+        ok = False
+    return ok
+
+
+if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=5000, debug=True)
     APP.logger.info("Server started")
 
@@ -157,4 +150,3 @@ if __name__ == '__main__':
         raise Exception(f"Failed to find or create defaults at {CONFIG_PATH}")
 
     fetch_config()
-
